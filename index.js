0e2fc6f52b2f70cc67f1b1c0537dc18bebb9a095
lgylgy1919 <59249964+lgylgy1919@users.noreply.github.com>
2020-07-20 15:55:17 +0900

    ExpressJS(7)

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-// 파일 목록 및 node module로부터 express를 요구(require)한다. import를 해도 된다.
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 // application을 만든다.
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on : http://localhost:${PORT}`);
-}
+const handleHome = (req, res) => res.send("Hello from my home");
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+//쿠키에 유저정보를 저장한다.
+app.use(cookieParser());
+//req에 포함된 form기입값을 가져온다.(서버가 json, urlencoded 등을 이해하도록 설정)
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({ extended: true }));
+//앱 보안을 강화하는 미들웨어
+app.use(helmet());
+//log 정보를 생성하는 미들웨어
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 
 //port=4000에 접근하면, 콜백함수로 handleListening을 실행한다.
 app.listen(PORT, handleListening);
