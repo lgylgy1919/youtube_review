3bfd4e7014fd757d6b1dbc4771b24b12ec30b4ce
lgylgy1919 <59249964+lgylgy1919@users.noreply.github.com>
2020-08-03 23:41:20 +0900

    aws(2)

--- middlewares.js
+++ middlewares.js
@@ -1,8 +1,32 @@
-import routes from "./routes";
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+import routes from "./routes";
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+  region: "ap-northeast-1",
+});
+
 // multer?? 파일 업로드에 필요.
-const multerVideo = multer({ dest: "uploads/videos/" });
-const multerAvatar = multer({ dest: "uploads/avatars/" });
+const multerVideo = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "youtube0720/video",
+  }),
+});
+const multerAvatar = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "youtube0720/avatar",
+  }),
+});
+//Single은 하나의 파일만 업로드하겠다는 의미, videoRouter에서 미들웨어로 사용
+export const uploadVideo = multerVideo.single("videoFile");
+export const uploadAvatar = multerAvatar.single("avatar");
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Youtube";
@@ -27,7 +51,3 @@ export const onlyPrivate = (req, res, next) => {
     res.redirect(routes.home);
   }
 };
-
-//Single은 하나의 파일만 업로드하겠다는 의미, videoRouter에서 미들웨어로 사용
-export const uploadVideo = multerVideo.single("videoFile");
-export const uploadAvatar = multerAvatar.single("avatar");
